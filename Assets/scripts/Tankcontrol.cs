29ae0836f222de1faa853d9d495a54150aaf6e23
Bartłomiej Borkowski <bartekborkowski13@gmail.com>
2020-11-03 14:49:04 +0100

    First Commit

---
+++ Assets/scripts/Tankcontrol.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tankcontrol : MonoBehaviour
+{
+
+    //Obiekt odpowiedzialny za ruch gracza.
+    public CharacterController characterControler;
+
+    public float predkoscPoruszania = 9.0f;
+    public float aktualnaWysokoscSkoku = 0f;
+    public float predkoscBiegania = 7.0f;
+    public float czuloscMyszki = 3.0f;
+    public float zakresMyszyGoraDol = 90.0f;
+    void Start()
+    {
+        characterControler = GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+        klawiatura();
+    }
+    private void klawiatura()
+    {
+        float rochPrzodTyl = Input.GetAxis("Vertical") * predkoscPoruszania;
+        float rochLewoPrawo = Input.GetAxis("Horizontal") * predkoscPoruszania * 0;
+        aktualnaWysokoscSkoku += Physics.gravity.y * Time.deltaTime;
+        Debug.Log(Physics.gravity.y);
+
+        if (Input.GetKeyDown("left shift"))
+        {
+            predkoscPoruszania += predkoscBiegania;
+        }
+        else if (Input.GetKeyUp("left shift"))
+        {
+            predkoscPoruszania -= predkoscBiegania;
+        }
+        Vector3 ruch = new Vector3(rochLewoPrawo, aktualnaWysokoscSkoku, rochPrzodTyl);
+        ruch = transform.rotation * ruch;
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * 20);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.down * Time.deltaTime * 20);
+        }
+
+        characterControler.Move(ruch * Time.deltaTime);
+    }
+}
---
+++ Assets/scripts/Wierza.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wierza : MonoBehaviour
+{
+
+    //Obiekt odpowiedzialny za ruch gracza.
+    public CharacterController characterControler;
+
+    public float predkoscPoruszania = 9.0f;
+    public float aktualnaWysokoscSkoku = 0f;
+    public float predkoscBiegania = 7.0f;
+    public float czuloscMyszki = 3.0f;
+    public float zakresMyszyGoraDol = 90.0f;
+    void Start()
+    {
+        characterControler = GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+        myszka();
+    }
+    private void myszka()
+    {
+      
+            float myszLewoPrawo = Input.GetAxis("Mouse X") * czuloscMyszki;
+            transform.Rotate(0, myszLewoPrawo, 0);
+ 
+    }
+}
